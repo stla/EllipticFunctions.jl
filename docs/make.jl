77b7d5daf673f11c238bacf7c0038932b0f1d5f3
stla <laurent_step@yahoo.fr>
2023-10-24 00:33:28 +0200

    updated docs folder

--- docs/make.jl
+++ docs/make.jl
@@ -3,3 +3,7 @@ push!(LOAD_PATH,"../src/")
 using Documenter, EllipticFunctions
 
 makedocs(sitename="EllipticFunctions", modules = [EllipticFunctions])
+
+deploydocs(
+    repo = "github.com/stla/EllipticFunctions.jl.git",
+)
